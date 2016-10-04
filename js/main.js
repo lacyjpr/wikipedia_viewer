40cfb67a8176b2071a53684959e9a9ee400733bb
Jean-Paul Lacy <lacyjpr@gmail.com>
2016-10-04 09:18:02 -0700

    fix form interference with ajax

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
-function search() {
+$("#form-container").submit(function(e) {
+	e.preventDefault();
 	var searchTerm = $("#searchText").val();
 
 	var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=wikiCallback";
 	console.log(wikiUrl);
+
 	$.ajax({
 		type: "GET",
 		url: wikiUrl,
-		dataType: "json",
+		dataType: "jsonp",
 		headers: {
 			"Api-User-Agent": "Example/1.0"
 		},
 		success: function(response){
 			console.log(response);
-		};
+		}
 	});
-}
+});
 
-$("#form-container").submit(search);
+
 
