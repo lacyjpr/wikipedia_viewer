3be732ac40e369f2c5479aafec00217ca8c607aa
Jean-Paul Lacy <lacyjpr@gmail.com>
2016-10-04 00:12:11 -0700

    add broken search functionality

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-var searchTerm = "water skiing";
+function search() {
+	var searchTerm = $("#searchText").val();
 
-var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=wikiCallback"
-
+	var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=wikiCallback";
+	console.log(wikiUrl);
 	$.ajax({
+		type: "GET",
 		url: wikiUrl,
-		dataType: "jsonp",
-		// jsonp: "callback",
-		success: function(response) {
+		dataType: "json",
+		headers: {
+			"Api-User-Agent": "Example/1.0"
+		},
+		success: function(response){
 			console.log(response);
-		}
+		};
 	});
+}
+
+$("#form-container").submit(search);
+
