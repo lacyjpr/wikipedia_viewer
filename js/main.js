a950dba50510ad69c349e73fb530d6ca4ead21c3
Jean-Paul Lacy <lacyjpr@gmail.com>
2016-10-06 11:21:07 -0700

    switch api search to do text searches

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $("#form-container").submit(function(event) {
 	event.preventDefault();
 	var searchTerm = $("#searchText").val();
-
-	var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=wikiCallback";
+//"https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback?";
+	var wikiUrl = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + searchTerm + "&format=json";
 
 	$.ajax({
 		type: "GET",
@@ -12,14 +12,15 @@ $("#form-container").submit(function(event) {
 			"Api-User-Agent": "Example/1.0"
 		},
 		success: function(response){
+			console.log(response);
 			// Remove previous results
 			$("#results").html("");
 			// Loop through response array and append results
-			if (response[1].length === 0) {
+			if (response.query.search.length === 0) {
 				$("#results").append('<div class="text-center result"><h4>No Results Found</h4></div>');
 			} else {
-				for (var i = 0; i < response[1].length; i++) {
-					$("#results").append('<div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-8 col-xs-offset-2 result"><a href=' + response[3][i] + ' target="blank_">' + response[1][i] + '</a><p class="description">' + response[2][i] + '</div');
+				for (var i = 0; i < response.query.search.length; i++) {
+					$("#results").append('<div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-8 col-xs-offset-2 result"><a href=http://en.wikipedia.org/wiki/' + response.query.search[i].title + ' target="blank_">' + response.query.search[i].title + '</a><p class="description">' + response.query.search[i].snippet + '</div');
 				}
 			}
 		},
