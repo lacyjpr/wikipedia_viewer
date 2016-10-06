9575c9c9023b9db1cbfbf6627719a6b253ff0235
Jean-Paul Lacy <lacyjpr@gmail.com>
2016-10-06 12:35:58 -0700

    clean up code

--- js/main.js
+++ js/main.js
@@ -14,12 +14,12 @@ $("#form-container").submit(function(event) {
 			console.log(response)
 			// Remove previous results
 			$("#results").html("");
-			// Loop through response array and append results
 			if (response.hasOwnProperty("error")) {
-				$("#results").append('<div class="text-center result"><h4>No Results Found</h4></div>');
+				$("#results").append('<div class="text-center result"><h4>Error encountered. Please try again later!</h4></div>');
 			} else if (response.query.search.length === 0){
 				$("#results").append('<div class="text-center result"><h4>No Results Found</h4></div>');
 			} else {
+				// Loop through response array and append results
 				for (var i = 0; i < response.query.search.length; i++) {
 					$("#results").append('<div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-8 col-xs-offset-2 result"><a href=http://en.wikipedia.org/wiki/' + response.query.search[i].title + ' target="blank_">' + response.query.search[i].title + '</a><p class="description">' + response.query.search[i].snippet + '</div');
 				}
