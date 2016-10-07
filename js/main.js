bdacd9a6aafeb4f93b48e6ebbd6d6b3283dd0f8d
Jean-Paul Lacy <lacyjpr@gmail.com>
2016-10-06 19:45:37 -0700

    fix error message width

--- js/main.js
+++ js/main.js
@@ -15,10 +15,10 @@ $("#form-container").submit(function(event) {
 			$("#results").html("");
 			// Check for errors
 			if (response.hasOwnProperty("error")) {
-				$("#results").append('<div class="text-center result"><h4>Error encountered. Please try again later!</h4></div>');
+				$("#results").append('<div class="text-center col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-8 col-xs-offset-2 result"><h4>Error encountered. Please try again later!</h4></div>');
 			// Check for no results
 			} else if (response.query.search.length === 0){
-				$("#results").append('<div class="text-center result"><h4>No Results Found</h4></div>');
+				$("#results").append('<div class="text-center col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-8 col-xs-offset-2 result"><h4>No Results Found</h4></div>');
 			} else {
 				var result = response.query.search;
 				// Loop through response array and append results
