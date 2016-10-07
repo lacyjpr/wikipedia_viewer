69569e4aa4ec3f66c31b7b93f5b87ab97b205c7c
Jean-Paul Lacy <lacyjpr@gmail.com>
2016-10-06 17:27:37 -0700

    clean up code

--- js/main.js
+++ js/main.js
@@ -11,17 +11,23 @@ $("#form-container").submit(function(event) {
 			"Api-User-Agent": "Example/1.0"
 		},
 		success: function(response){
-			console.log(response)
+			console.log(response);
 			// Remove previous results
 			$("#results").html("");
+			// Check for errors
 			if (response.hasOwnProperty("error")) {
 				$("#results").append('<div class="text-center result"><h4>Error encountered. Please try again later!</h4></div>');
+			// Check for no results
 			} else if (response.query.search.length === 0){
 				$("#results").append('<div class="text-center result"><h4>No Results Found</h4></div>');
 			} else {
+				var result = response.query.search;
 				// Loop through response array and append results
-				for (var i = 0; i < response.query.search.length; i++) {
-					$("#results").append('<div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-8 col-xs-offset-2 result"><a href=http://en.wikipedia.org/wiki/' + response.query.search[i].title + ' target="blank_">' + response.query.search[i].title + '</a><p class="description">' + response.query.search[i].snippet + '</div');
+				for (var i = 0; i < result.length; i++) {
+					var endUrl = result[i].title.replace(/ /g, "_");
+					var title = result[i].title;
+					var snippet = result[i].snippet;
+					$("#results").append('<div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-8 col-xs-offset-2 result"><a href=http://en.wikipedia.org/wiki/' + endUrl + ' target="blank_">' + title + '</a><p class="description">' + snippet + '</div');
 				}
 			}
 		},
